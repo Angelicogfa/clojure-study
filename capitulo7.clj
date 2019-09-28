d8b96cd4d5b8bb7878878db773ce79f3aed710d5
angelicogfa <angelicogfa@gmail.com>
2019-09-28 16:48:06 -0300

    inclusao do capitulo 7

--- capitulo7.clj
+++ capitulo7.clj
@@ -77,3 +77,86 @@ registros
 
 (swap! registros conj
        {:valor 2700M :tipo "despesa" :comentario "Bico" :moeda "R$" :data "01/12/2016"})
+
+
+(defn registrar [transacao]
+  (swap! registros conj transacao))
+
+(registrar {:valor 33M :tipo "despesa" :comentario "Almoço" :moeda "R$" :data "19/11/2016"})
+(registrar {:valor 45M :tipo "despesa" :comentario "Jogo do steam" :moeda "R$" :data "26/12/2016"})
+
+(def transacoes @registros)
+transacoes
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn despesa? [transacao]
+  (= (:tipo transacao) "despesa"))
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (saldo-acumulado (if (despesa? transacao)
+                       (- acumulado (:valor transacao))
+                       (+ acumulado (:valor transacao)))
+                     (rest transacoes))
+    acumulado))
+
+(saldo-acumulado 0 transacoes)
+(saldo-acumulado 0 ())
+(saldo-acumulado 0 (take 2 transacoes))
+
+(defn calcular [acumulado transacao]
+  (let [valor (:valor transacao)]
+    (if (despesa? transacao)
+      (- acumulado valor)
+      (+ acumulado valor))))
+
+(defn saldo-acumulado [acumulado transacoes]
+  (if-let [transacao (first transacoes)]
+    (do
+      (prn "Começou saldo-acumulado. Saldo até agora: " acumulado)
+      (saldo-acumulado (calcular acumulado transacao)
+                       (rest transacoes)))
+
+    (do
+      (prn "Processo encerrado. Saldo final: " acumulado)
+      acumulado)))
+
+(saldo-acumulado 0 transacoes)
+
+(defn saldo [transacoes]
+  (saldo-acumulado 0 transacoes))
+
+(saldo transacoes)
+
+(defn saldo
+  ([transacoes]
+   (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if-let [transacao (first transacoes)]
+     (saldo (calcular acumulado transacao) (rest transacoes))
+     acumulado)))
+
+(saldo transacoes)
+(saldo 0 transacoes)
+
+(defn como-transacao [valor]
+  {:valor valor})
+
+(def poucas-transacoes (map como-transacao (range 10)))
+(def muitas-transacoes (map como-transacao (range 100)))
+(def incontaveis-transacoes (map como-transacao (range 100000)))
+
+(saldo poucas-transacoes)
+(saldo muitas-transacoes)
+(saldo incontaveis-transacoes)
+
+(defn saldo
+  ([transacoes] (saldo 0 transacoes))
+  ([acumulado transacoes]
+   (if (empty? transacoes)
+     acumulado
+     (recur (calcular acumulado (first transacoes))
+            (rest transacoes)))))
+
+(reduce calcular 0 transacoes)
